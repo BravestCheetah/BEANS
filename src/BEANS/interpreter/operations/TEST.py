2fb4952b4287fbbc42b8e935ed115845a1eabc7d
CheetahDoesStuff <contact.cheetah@tutamail.com>
2025-07-30 17:56:03 +0200

    changed name to test.py

--- src/BEANS/interpreter/operations/TEST.py
+++ src/BEANS/interpreter/operations/TEST.py
@@ -1,3 +1,3 @@
 class operation:
     def execute_operation(self, mem, *args):
-        print(f"LDI got called with the args {str(args)}")
+        print(f"TEST got called with the args {str(args)}")

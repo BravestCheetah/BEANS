ab9ebef47dee988d792fb7022e1d6efd472cde99
CheetahDoesStuff <contact.cheetah@tutamail.com>
2025-08-15 17:28:57 +0200

    window :O

--- src/BEANS/BEANS_GUI/gui.py
+++ src/BEANS/BEANS_GUI/gui.py
@@ -1,6 +1,14 @@
-from PyQt6.QtWidgets import QApplication, QMainWindow
-app = QApplication([])
-window = QMainWindow()
-window.show()
+from PyQt6.QtCore import QSize, Qt
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QGridLayout
 
+class beans_gui(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("BEANS 0.1.0 - /path/to/file.bean")
+
+
+app = QApplication([])
+window = beans_gui()
+window.show()
 app.exec()

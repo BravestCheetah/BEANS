3608f0e9c2dedc56547093b8e83461207f1c703c
CheetahDoesStuff <contact.cheetah@tutamail.com>
2025-08-15 20:24:12 +0200

    title is now modular

--- src/BEANS/BEANS_GUI/gui.py
+++ src/BEANS/BEANS_GUI/gui.py
@@ -2,13 +2,13 @@ from PyQt6.QtCore import QSize, Qt
 from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QGridLayout
 
 class beans_gui(QMainWindow):
-    def __init__(self):
+    def __init__(self, code_path):
         super().__init__()
 
-        self.setWindowTitle("BEANS 0.1.0 - /path/to/file.bean")
+        self.setWindowTitle(f"BEANS 0.1.0 -  {code_path}")
 
 
 app = QApplication([])
-window = beans_gui()
+window = beans_gui("Path/to/code.bean")
 window.show()
 app.exec()

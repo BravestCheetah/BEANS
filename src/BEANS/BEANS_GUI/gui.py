ebad36886880f6a58551317f2fccdda4bb1bc48e
CheetahDoesStuff <contact.cheetah@tutamail.com>
2025-08-16 00:48:00 +0200

    we got grid babyyyyyy

--- src/BEANS/BEANS_GUI/gui.py
+++ src/BEANS/BEANS_GUI/gui.py
@@ -1,11 +1,23 @@
 from PyQt6.QtCore import QSize, Qt
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QGridLayout
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QGridLayout, QWidget
 
 class beans_gui(QMainWindow):
     def __init__(self, code_path):
         super().__init__()
 
         self.setWindowTitle(f"BEANS 0.1.0 -  {code_path}")
+        self.setGeometry(400, 400, 600, 300)
+
+        central = QWidget()
+        self.setCentralWidget(central)
+
+        grid = QGridLayout()
+        central.setLayout(grid)
+
+        for i in range(1, 5):
+            for j in range(1, 5):
+                grid.addWidget(QPushButton("B" + str(i) + str(j)), i, j)
+
 
 
 app = QApplication([])

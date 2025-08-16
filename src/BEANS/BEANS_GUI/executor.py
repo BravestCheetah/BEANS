2cf0b4aed29b80112facb08bae41f6bf65b73499
CheetahDoesStuff <contact.cheetah@tutamail.com>
2025-08-16 11:10:16 +0200

    adding exectuor file

---
+++ src/BEANS/BEANS_GUI/executor.py
@@ -0,0 +1,7 @@
+from sys import argv
+
+from src.BEANS.BEANS_GUI.gui import BEANSGui
+from src.BEANS.interpreter.interpreter import interpret_line
+from src.BEANS.interpreter.memory.memory import memory
+from src.BEANS.interpreter.memory.registers import registers
+

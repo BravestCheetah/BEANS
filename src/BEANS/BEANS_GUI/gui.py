4b4cb52bec2609b2a13d625339c569c950eb37c3
CheetahDoesStuff <contact.cheetah@tutamail.com>
2025-08-16 02:33:38 +0200

    contantsies

--- src/BEANS/BEANS_GUI/gui.py
+++ src/BEANS/BEANS_GUI/gui.py
@@ -1,4 +1,5 @@
-from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QGroupBox
+from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QGroupBox, QLabel, QSpacerItem, QSizePolicy
+from PyQt6.QtCore import Qt
 
 class BeansGui(QMainWindow):
     def __init__(self, code_path):
@@ -16,8 +17,24 @@ class BeansGui(QMainWindow):
 
 
 
-        operation_panel = QGroupBox("OPERATION")
 
+        operation_panel = QGroupBox("OPERATION")
+        operation_layout = QVBoxLayout()
+        operation_layout.setSpacing(2)
+        operation_layout.setContentsMargins(5, 5, 5, 5)
+        operation_panel.setLayout(operation_layout)
+
+        self.label_last_op = QLabel("Last Operation: None")
+        operation_layout.addWidget(self.label_last_op, alignment=Qt.AlignmentFlag.AlignTop | Qt.AlignmentFlag.AlignLeft)
+
+        self.label_last_args = QLabel("Last Arguments: [None, None]")
+        operation_layout.addWidget(self.label_last_args, alignment=Qt.AlignmentFlag.AlignTop | Qt.AlignmentFlag.AlignLeft)
+
+        self.label_last_op_line = QLabel("Line: None / None")
+        operation_layout.addWidget(self.label_last_op_line, alignment=Qt.AlignmentFlag.AlignTop | Qt.AlignmentFlag.AlignLeft)
+
+        self.label_last_op_time = QLabel("Time Taken: None")
+        operation_layout.addWidget(self.label_last_op_time, alignment=Qt.AlignmentFlag.AlignTop | Qt.AlignmentFlag.AlignLeft)
 
 
         top_layout.addWidget(operation_panel)
@@ -25,8 +42,8 @@ class BeansGui(QMainWindow):
 
 
         memory_panel = QGroupBox("MEMORY")
-
-
+        memory_layout = QVBoxLayout()
+        memory_panel.setLayout(memory_layout)
 
         top_layout.addWidget(memory_panel)
         main_layout.addLayout(top_layout)
@@ -34,7 +51,8 @@ class BeansGui(QMainWindow):
 
 
         module_panel = QGroupBox("I/O MODULES")
-
+        module_layout = QVBoxLayout()
+        module_panel.setLayout(module_layout)
 
         
         main_layout.addWidget(module_panel)

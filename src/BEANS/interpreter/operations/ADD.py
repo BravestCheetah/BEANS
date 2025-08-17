91469d032a98e47cd9b29d53bcd7e481677ce52f
CheetahDoesStuff <contact.cheetah@tutamail.com>
2025-08-18 00:19:33 +0200

    add add

--- src/BEANS/interpreter/operations/ADD.py
+++ src/BEANS/interpreter/operations/ADD.py
@@ -5,11 +5,11 @@ from src.BEANS.interpreter.memory.registers import registers
 class operation:
     def execute_operation(mem: tuple[registers, memory], args, pc_index): # type: ignore
 
-        if len(args) != 2 or args[0][0] != "reg" or args[1][0] != "reg" or args[2][0] != "reg":
+        if len(args) != 3 or args[0][0] != "reg" or args[1][0] != "reg" or args[2][0] != "reg":
             print("Wrong args lil bro")
         
         else:
-            value = int(mem[0].read(args[0][0])) + int(mem[0].read(args[1][0]))
+            value = int(mem[0].read(int(args[0][1]))) + int(mem[0].read(int(args[1][1])))
             mem[0].write(int(args[2][1]), value) # type: ignore
 
         return pc_index + 1

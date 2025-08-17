472d12d9acae75072662590209f83a0baa2513f8
CheetahDoesStuff <contact.cheetah@tutamail.com>
2025-08-17 21:48:40 +0200

    added ldi

--- src/BEANS/interpreter/operations/LDI.py
+++ src/BEANS/interpreter/operations/LDI.py
@@ -1,6 +1,10 @@
 class operation:
-    def execute_operation(mem, args, pc_index):
+    def execute_operation(mem, args, pc_index): # type: ignore
 
+        if len(args) != 2 or args[0][0] != "reg" or args[1][0] != "int":
+            print("Wrong args lil bro")
         
+        else:
+            mem[0].write(int(args[0][1]), int(args[1][1])) # type: ignore
 
         return pc_index + 1

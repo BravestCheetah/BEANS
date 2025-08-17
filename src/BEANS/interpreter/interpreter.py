5de446d7c432062e370e01079840bfa80a89d4ed
CheetahDoesStuff <contact.cheetah@tutamail.com>
2025-08-17 20:42:44 +0200

    argparsing lessgo

--- src/BEANS/interpreter/interpreter.py
+++ src/BEANS/interpreter/interpreter.py
@@ -2,6 +2,26 @@ from src.BEANS.interpreter.memory.registers import registers
 from src.BEANS.interpreter.memory.memory import memory
 from src.BEANS.interpreter.operation_handler import execute_operation
 
+def parse_args(args: list[str]) -> list:
+    
+    parsed = []
+
+    for arg in args:
+        if arg.startswith("r"):
+            parsed.append(("reg", arg[1:]))
+        
+        elif arg.startswith("m"):
+            parsed.append(("mem", arg[1:]))
+
+        elif arg.startswith("io"):
+            parsed.append(("io", args[1:]))
+
+        elif arg.startswith("i"):
+            parsed.append(("int", args[1:]))
+        
+
+    return parsed
+
 def interpret_line(line: str, regs: registers, mem: memory, pc_index: int):
     code = line.split(' ')
-    return execute_operation(code[0], (regs, mem), code[1:], pc_index)
+    return execute_operation(code[0], (regs, mem), parse_args(code[1:]), pc_index)
---
+++ src/BEANS/interpreter/operations/LDI.py
@@ -0,0 +1,6 @@
+class operation:
+    def execute_operation(mem, args, pc_index):
+
+        
+
+        return pc_index + 1

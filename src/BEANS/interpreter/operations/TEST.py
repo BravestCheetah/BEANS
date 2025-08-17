5bca75b8914938e33ee3ebf1e2614e9272b53c3c
CheetahDoesStuff <contact.cheetah@tutamail.com>
2025-08-17 02:29:08 +0200

    porgram counter

--- src/BEANS/interpreter/operations/TEST.py
+++ src/BEANS/interpreter/operations/TEST.py
@@ -1,3 +1,4 @@
 class operation:
-    def execute_operation(self, mem, args):
+    def execute_operation(self, mem, args, pc_index):
         print(f"TEST got called with the args {str(args)}")
+        return pc_index + 1

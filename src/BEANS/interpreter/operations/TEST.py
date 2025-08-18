5ab8c8a5049d376b577e0cea4024097f9abe8ce0
CheetahDoesStuff <contact.cheetah@tutamail.com>
2025-08-18 17:55:08 +0200

    uopdated test

--- src/BEANS/interpreter/operations/TEST.py
+++ src/BEANS/interpreter/operations/TEST.py
@@ -1,12 +1,12 @@
 from src.BEANS.interpreter.memory.memory import memory
 from src.BEANS.interpreter.memory.registers import registers
 
-from src.BEANS.interpreter.op_api import check_args, get_args
+from src.BEANS.interpreter.op_api import handle_args
 
 
 class operation:
 
     @staticmethod
     def execute_operation(mem: tuple[registers, memory], args, pc_index): # type: ignore
-        print(f"TEST got called! Argument 1 is num: {check_args(args, ["num"])} with value {get_args(args)}. Raw arguments: {args}")
+        print(f"TEST got called!")
         return pc_index + 1

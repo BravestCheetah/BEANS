b4455fa78e600494027dffd0c29b2b1806d4faa5
CheetahDoesStuff <contact.cheetah@tutamail.com>
2025-08-18 15:23:17 +0200

    rewrote ldi into the new arg handling api

--- src/BEANS/interpreter/operations/LDI.py
+++ src/BEANS/interpreter/operations/LDI.py
@@ -1,14 +1,14 @@
 from src.BEANS.interpreter.memory.memory import memory
 from src.BEANS.interpreter.memory.registers import registers
 
+from src.BEANS.interpreter.op_api import handle_args
+
 
 class operation:
     def execute_operation(mem: tuple[registers, memory], args, pc_index): # type: ignore
 
-        if len(args) != 2 or args[0][0] != "reg" or args[1][0] != "int":
-            print("Wrong args lil bro")
-        
-        else:
-            mem[0].write(int(args[0][1]), int(args[1][1])) # type: ignore
+        args = handle_args(args, ["reg", "num"])
+
+        mem[0].write(args[0], args[1])
 
         return pc_index + 1

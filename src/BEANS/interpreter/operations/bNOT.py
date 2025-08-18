7d44d93a8a501074b6b461773a7ac332a9c29f01
CheetahDoesStuff <contact.cheetah@tutamail.com>
2025-08-18 18:05:01 +0200

    OP: Added bNOT bitwise operation

--- src/BEANS/interpreter/operations/bNOT.py
+++ src/BEANS/interpreter/operations/bNOT.py
@@ -7,12 +7,10 @@ from src.BEANS.interpreter.op_api import handle_args
 class operation:
     def execute_operation(mem: tuple[registers, memory], args, pc_index): # type: ignore
 
-        args = handle_args(args, ["reg", "reg", "reg"])
+        args = handle_args(args, ["reg", "reg"])
 
-        size = mem[0].regs[0].size
-        mask = (1 << size) - 1
-        value = (mem[0].read(args[0]) | mem[0].read(args[1])) & mask
+        value = ~mem[0].read(args[0])
 
-        mem[0].write(args[2], value)
+        mem[0].write(args[1], value)
 
         return pc_index + 1 

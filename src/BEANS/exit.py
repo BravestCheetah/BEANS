aaf18391fbaa76c16cf034620b15abbfdd4a2971
CheetahDoesStuff <contact.cheetah@tutamail.com>
2025-08-18 21:01:35 +0200

    fixed pylances stupidity

--- src/BEANS/exit.py
+++ src/BEANS/exit.py
@@ -3,5 +3,5 @@ import sys
 def exit(msg: str = "Exiting the program..."):
     if msg:
         import src.BEANS.logger_global as lg
-        lg.logger.critical(msg)
+        lg.logger.critical(msg) # type: ignore
     sys.exit(1)

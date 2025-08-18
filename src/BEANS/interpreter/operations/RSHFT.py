555f05de9e54298a1771b30362a3217e1551f819
CheetahDoesStuff <contact.cheetah@tutamail.com>
2025-08-18 18:35:36 +0200

    OP: Added RSHFT operation

---
+++ src/BEANS/interpreter/operations/RSHFT.py
@@ -0,0 +1,16 @@
+from src.BEANS.interpreter.memory.memory import memory
+from src.BEANS.interpreter.memory.registers import registers
+
+from src.BEANS.interpreter.op_api import handle_args
+
+
+class operation:
+    def execute_operation(mem: tuple[registers, memory], args, pc_index): # type: ignore
+
+        args = handle_args(args, ["reg", "reg", "reg"])
+
+        value = mem[0].read(args[0]) >> args[1]
+
+        mem[0].write(args[2], value)
+
+        return pc_index + 1 

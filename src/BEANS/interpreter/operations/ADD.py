b38bb5d59ec312527d2b2a41fba96086823c13aa
CheetahDoesStuff <contact.cheetah@tutamail.com>
2025-08-18 19:56:21 +0200

    rewrote add operation to follow new api

--- src/BEANS/interpreter/operations/ADD.py
+++ src/BEANS/interpreter/operations/ADD.py
@@ -1,15 +1,15 @@
 from src.BEANS.interpreter.memory.memory import memory
 from src.BEANS.interpreter.memory.registers import registers
 
+from src.BEANS.interpreter.op_api import handle_args
+
 
 class operation:
     def execute_operation(mem: tuple[registers, memory], args, pc_index): # type: ignore
 
-        if len(args) != 3 or args[0][0] != "reg" or args[1][0] != "reg" or args[2][0] != "reg":
-            print("Wrong args lil bro")
-        
-        else:
-            value = int(mem[0].read(int(args[0][1]))) + int(mem[0].read(int(args[1][1])))
-            mem[0].write(int(args[2][1]), value) # type: ignore
+        args = handle_args(args, ["reg", "reg", "reg"])
+
+        value = mem[0].read(args[0]) + mem[0].read(args[1])
+        mem[0].write(args[2], value)
 
         return pc_index + 1

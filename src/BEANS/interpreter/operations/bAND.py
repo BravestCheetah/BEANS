ea22de42e945c4fe216701e3c9dd3b62c20c17ba
CheetahDoesStuff <contact.cheetah@tutamail.com>
2025-08-18 19:11:43 +0200

    O: Added bAND Bitwise Operation

---
+++ src/BEANS/interpreter/operations/bAND.py
@@ -0,0 +1,19 @@
+from src.BEANS.interpreter.memory.memory import memory
+from src.BEANS.interpreter.memory.registers import registers
+
+from src.BEANS.interpreter.op_api import handle_args
+
+
+class operation:
+    def execute_operation(mem: tuple[registers, memory], args, pc_index): # type: ignore
+        print(args)
+
+        args = handle_args(args, ["reg", "reg", "reg"])
+
+        size = mem[0].regs[0].size
+        mask = (1 << size) - 1
+        value = (mem[0].read(args[0]) & mem[0].read(args[1])) & mask
+
+        mem[0].write(args[2], value)
+
+        return pc_index + 1

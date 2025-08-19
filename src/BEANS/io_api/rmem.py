a972389c6dab68c5998bfbb0ae842620d7b320cf
CheetahDoesStuff <contact.cheetah@tutamail.com>
2025-08-19 20:34:25 +0200

    added write to restricted memory

--- src/BEANS/io_api/rmem.py
+++ src/BEANS/io_api/rmem.py
@@ -3,11 +3,17 @@ from src.BEANS.interpreter.memory.memory import memory
 class restricted_memory:
     def __init__(self, memory: memory, access_adresses: list) -> None:
         self.mem = memory
-        self.access_addresses = access_adresses
+        self.access_adresses = access_adresses
 
-    def read(self, adress) -> int:
+    def read(self, adress: int) -> int:
 
-        if adress in self.access_addresses:
+        if adress in self.access_adresses:
             return self.mem.memory.read(adress)
         else:
             return 0
+    
+    def write(self, adress: int, value: int):
+        
+        if adress in self.access_adresses:
+            self.mem.memory.write(adress, value)
+        

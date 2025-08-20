b5430a8b11d1e9da4d50be561251dd1363b265af
CheetahDoesStuff <contact.cheetah@tutamail.com>
2025-08-20 02:20:26 +0200

    added restricted memory to LDIO

--- src/BEANS/interpreter/operations/LDIO.py
+++ src/BEANS/interpreter/operations/LDIO.py
@@ -1,5 +1,6 @@
 from src.BEANS.interpreter.memory.memory import memory
 from src.BEANS.interpreter.memory.registers import registers
+from src.BEANS.io_api.rmem import restricted_memory
 
 from src.BEANS.interpreter.op_api import handle_args
 
@@ -10,6 +11,8 @@ class operation:
         args = handle_args(args, ["num", "num", "num", "mem", "mem"])
         module_id = chr(args[0]) + chr(args[1]) + chr(args[2])
         memory_range = list(range(args[3], args[4] + 1))
+
+        rmem = restricted_memory(mem[1], memory_range)
 
 
         return pc_index + 1

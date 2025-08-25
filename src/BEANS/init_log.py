28dd2b6dbeb32ec1074d5d0ec7ace216402b2360
CheetahDoesStuff <contact.cheetah@tutamail.com>
2025-08-25 04:01:00 +0200

    stupid pylance go away

--- src/BEANS/init_log.py
+++ src/BEANS/init_log.py
@@ -2,7 +2,7 @@ import src.BEANS.logger_global as lg
 from platformdirs import user_data_dir
 
 def init_logs():
-    lg.logger.info("-------- BEANS ---------")
-    lg.logger.info(" - Beans Is Initializing - ")
-    lg.logger.info(f" - Beans IO Path: {user_data_dir("BEANS", "Cheetah")} - ")
-    lg.logger.info("-------- BEANS ---------")
+    lg.logger.info("-------- BEANS ---------") # type: ignore
+    lg.logger.info(" - Beans Is Initializing - ") # type: ignore
+    lg.logger.info(f" - Beans IO Path: {user_data_dir("BEANS", "Cheetah")} - ") # type: ignore
+    lg.logger.info("-------- BEANS ---------") # type: ignore

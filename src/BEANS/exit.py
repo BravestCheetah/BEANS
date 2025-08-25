81acbf1282e4a437a9c1a2b9c3bd1e1732d82616
CheetahDoesStuff <contact.cheetah@tutamail.com>
2025-08-25 22:16:22 +0200

    deelted exit.py

--- src/BEANS/exit.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-def exit(msg: str = "Exiting the program..."):
-    if msg:
-        import src.BEANS.logger_global as lg
-        lg.logger.critical(msg) # type: ignore
-    sys.exit(1)

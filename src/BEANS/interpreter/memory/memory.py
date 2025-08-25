e0c0c8a117228fbae1f02145c7796d1e58def0ca
CheetahDoesStuff <contact.cheetah@tutamail.com>
2025-08-25 04:17:00 +0200

    memory updated

--- src/BEANS/interpreter/memory/memory.py
+++ src/BEANS/interpreter/memory/memory.py
@@ -4,8 +4,8 @@ class memory():
     def __init__(self, mem_size: int, mem_reg_size: int):
         self.memory = registers(mem_reg_size, mem_size)
     
-    def read(self, address: int, dest_reg: register):
-        dest_reg.set(int(self.memory.read(address), 2))
+    def read(self, address: int):
+        return self.memory.read(address)
     
     def write(self, adress: int, val_reg: register):
         self.memory.write(adress, val_reg.value())
